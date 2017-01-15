737b1f6be1644ffc3a156ae270b288ae51029e56
Pawel Pietrasz <szpght@gmail.com>
2017-01-15 20:05:38 +0100

    example path in test model

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -23,12 +23,12 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    DataVector data;
+    DataVector data, path;
     data.push_back(DataPoint(2, 1));
     data.push_back(DataPoint(1, 2));
     data.push_back(DataPoint(1, 3));
-    data.push_back(DataPoint(1.5, 0));
-    data.push_back(DataPoint(4, 4));
+    path.push_back(DataPoint(1.5, 0));
+	path.push_back(DataPoint(4, 4));
     IntVector clusters;
     clusters.push_back(0);
     clusters.push_back(1);
@@ -38,7 +38,8 @@ void MainWindow::on_pushButton_clicked()
     ViewModel model;
     model.Data = data;
     model.Clusters = clusters;
-    model.K = 4;
+    model.K = 3;
+	model.GroupPaths.push_back(path);
     plot->model = &model;
     plot->UpdatePlot();
 }

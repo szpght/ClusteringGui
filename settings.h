b384c2a08a0e2a60f906509ed935c35e8b9c2571
Pawel Pietrasz <szpght@gmail.com>
2017-01-22 22:42:47 +0100

    settings.h

---
+++ settings.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace Settings
+{
+    const double RangeXMin = 0;
+    const double RangeXMax = 5;
+    const double RangeYMin = 0;
+    const double RangeYMax = 5;
+    const bool FixedAxes = true;
+}

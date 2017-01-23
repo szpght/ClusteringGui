8f86f9d542ac55c2a5e0eea56d47ea4e7671bfda
Paweł Pietrasz <szpght@gmail.com>
2017-01-23 02:23:05 +0100

    initialize rand()

--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@
 #include "stdafx.h"
 #include  "DataPoint.h"
 #include "KMeansClustering.h"
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 
 int main(int argc, char *argv[])
 {
+    srand(time(NULL));
     QApplication a(argc, argv);
     MainWindow w(0);
     w.show();
